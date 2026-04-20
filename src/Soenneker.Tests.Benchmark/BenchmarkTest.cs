a3ee6718cd85b2dc253981f7b3b06a7765312a0f
Jake Soenneker <jake@soenneker.com>
2026-04-20 10:42:41 -0500

    -> TUnit

--- src/Soenneker.Tests.Benchmark/BenchmarkTest.cs
+++ src/Soenneker.Tests.Benchmark/BenchmarkTest.cs
@@ -1,8 +1,6 @@
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Reports;
-using System.Threading;
-using Xunit;
 
 namespace Soenneker.Tests.Benchmark;
 
@@ -13,14 +11,8 @@ public abstract class BenchmarkTest
 {
     protected ManualConfig DefaultConf { get; }
 
-    protected ITestOutputHelper OutputHelper { get; set; }
-
-    protected CancellationToken CancellationToken => TestContext.Current.CancellationToken;
-
-    protected BenchmarkTest(ITestOutputHelper outputHelper)
+    protected BenchmarkTest()
     {
-        OutputHelper = outputHelper;
-
         DefaultConf = ManualConfig.Create(DefaultConfig.Instance).WithOptions(ConfigOptions.DisableOptimizationsValidator);
         DefaultConf.SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend);
     }
